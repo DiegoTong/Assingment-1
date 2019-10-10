c5193389c4c73d645b16fb37c660383cebc8921f
Diego Andres Tong Calix <didres3217@gmail.com>
2019-10-10 12:48:46 +0100

    Added an update function to Enemy

--- Assignment-1/Assignment-1/Enemy.cpp
+++ Assignment-1/Assignment-1/Enemy.cpp
@@ -1,1 +1,31 @@
 #include "Enemy.h"
+
+void update()
+{
+	// Movement Randomizer
+	srand(time(NULL));
+	int randNum = rand() % 10+1;
+	if (randNum <9)
+	{
+		// Move x by 1
+	}
+	else
+	{
+		// move x reverse
+	}
+
+	srand(time(NULL));
+	randNum = rand() % 10 + 1;
+	if (randNum < 9)
+	{
+		// Move y by 1
+	}
+	else
+	{
+		// move y reverse
+	}
+
+	/*
+	If deduct from health, character multiply by 3
+	*/
+}
--- Assignment-1/Assignment-1/Enemy.h
+++ Assignment-1/Assignment-1/Enemy.h
@@ -2,6 +2,8 @@
 #include "GameCharacter.h"
 class Enemy : public GameCharacter
 {
+	
+public:
 	virtual void update() const;
 };
 
